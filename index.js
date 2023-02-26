291bd914e0ae834a196d7ed7028b0c4724a4483a
ahmed <ahmedtalha.miah@gmail.com>
2023-02-26 11:50:20 +0000

    initialized git repository

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function openMenu {
+function openMenu() {
     document.body.classList += " menu--open"
 }
 
-function closeMenu {
-    document.body.classList.remove (' menu--open')
+function closeMenu() {
+    document.body.classList.remove ('menu--open')
 }
 
